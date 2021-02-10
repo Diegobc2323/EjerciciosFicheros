2b7cb31f046dd9501818de554e7300757f9b65b0
Diego <alum22cassasdiego@cpifpbajoaragon.com>
2021-02-10 14:18:09 +0100

    actualizado para que los metodos funcionen bien

--- src/Ficheros/Main.java
+++ src/Ficheros/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		IODatos.mostrar("alumnos.txt", 110);				//parte 1
+		//IODatos.mostrar("alumnos.txt", 110);				//parte 1
 		
 		//System.out.println(IODatos.buscar());				//parte 2		
 		
